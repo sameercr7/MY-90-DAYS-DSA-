54033c181a81264054f329c79f98dbe7b5ea73dc
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-07 23:24:05 +0530

    Update 2sum.cpp

--- DAY-2/2sum.cpp
+++ DAY-2/2sum.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #two sum problem 
 #make a hash table in which key and value is both stored if it is not in the map  if (mp.find(target - nums[i]) != mp.end()) then just update the map with its value
 #and if found the eturn the answer simple
@@ -23,3 +24,30 @@ vector<int> twoSum(vector<int>& nums, int target) {
 
     return res;
 }
+=======
+#two sum problem 
+#make a hash table in which key and value is both stored if it is not in the map  if (mp.find(target - nums[i]) != mp.end()) then just update the map with its value
+#and if found the eturn the answer simple
+
+
+
+vector<int> twoSum(vector<int>& nums, int target) {
+
+    vector<int> res;
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); ++i) {
+
+   	 if (mp.find(target - nums[i]) != mp.end()) {
+
+   		 res.push_back(i);
+   		 res.push_back(mp[target - nums[i]]);
+   		 return res;
+   	 }
+
+   	 mp[nums[i]] = i;
+    }
+
+    return res;
+}
+>>>>>>> 5c800002a20a931073c1389272d3833f0f1eac38
