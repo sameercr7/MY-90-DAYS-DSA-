d29c5bbfcee79fc812f2ef99b2104ace3dd74618
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-07 23:24:21 +0530

    Update trial2.cpp

--- DAY-3/ARRAY PART-1/trial2.cpp
+++ DAY-3/ARRAY PART-1/trial2.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //naive approach of 2nd largest number
 //function within function 
 #include<bits/stdc++.h>
@@ -28,3 +29,35 @@ int main(){
     cout<<secondlargest(arr,5);
     return 0;
 }
+=======
+//naive approach of 2nd largest number
+//function within function 
+#include<bits/stdc++.h>
+using namespace std;
+int secondlargest(int arr[],int n){
+    
+    int res=-1;
+    int largest=0;
+    for(int i=1;i<n;i++)
+        if(arr[i]>arr[largest])
+            largest=i;
+    cout<<arr[largest];
+    cout<<endl;
+
+   for(int i=0;i<n;i++){
+        if(arr[i]!=arr[largest]){
+            if(res==-1)
+            res=i;
+            else if(arr[i]>arr[res])
+            res=i;
+        }
+    }
+    return res;
+}
+
+int main(){
+    int arr[]={5,20,12,20,10};
+    cout<<secondlargest(arr,5);
+    return 0;
+}
+>>>>>>> 5c800002a20a931073c1389272d3833f0f1eac38
