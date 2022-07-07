3bbf2ca65f400c8cbb60c126118f9d297cedb43e
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-07 23:24:09 +0530

    Update maps.cpp

--- DAY-2/maps.cpp
+++ DAY-2/maps.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #create key to value mapping
 #not continuous just they arein pair
 #store the value in sorted order
@@ -5,4 +6,13 @@
 #e.g m[5]='abc' m[5]="cde"so o/p will be 5 cde
 #complexity depends on keys s,size()*log(n)
 #unordered (no maintainance of order)
+=======
+#create key to value mapping
+#not continuous just they arein pair
+#store the value in sorted order
+#we cannnot insert dupicate key in map it will replace its current value by old value 
+#e.g m[5]='abc' m[5]="cde"so o/p will be 5 cde
+#complexity depends on keys s,size()*log(n)
+#unordered (no maintainance of order)
+>>>>>>> 5c800002a20a931073c1389272d3833f0f1eac38
 #uses hash table so complexity is  o(1 )
