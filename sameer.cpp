0a296ca1c2e1cc5959f36957d7cb7f20ae775e1c
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-07 23:26:58 +0530

    Create sameer.cpp

---
+++ sameer.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int a;
+   char b;
+   cin>>a>>b;
+
+    cout<<"hello"<<" "<<a<<" "<<b;
+}
+#it only take only one value bcoz it is a character
+#456 7789
+o/p hello 456 7
+#return 0 sirf yhi btata h ki code ab complete hochuka h
