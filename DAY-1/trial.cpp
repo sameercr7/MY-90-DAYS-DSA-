5c74825a48f77e850e6ab94cd169951afcb9f213
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-07 23:24:02 +0530

    Update trial.cpp

--- DAY-1/trial.cpp
+++ DAY-1/trial.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<bits/stdc++.h>
 using namespace std;
 void sameer(int &a,int &b)
@@ -13,4 +14,21 @@ int main(){
      cout<<a<<" "<<b<<endl;
     sameer(a,b);
     cout<<a<<" "<<b;
+=======
+#include<bits/stdc++.h>
+using namespace std;
+void sameer(int &a,int &b)
+{
+    int temp=a;
+    a=b;
+    b=temp;
+
+}
+int main(){
+    int b=3;
+    int a=4;
+     cout<<a<<" "<<b<<endl;
+    sameer(a,b);
+    cout<<a<<" "<<b;
+>>>>>>> 5c800002a20a931073c1389272d3833f0f1eac38
 }
