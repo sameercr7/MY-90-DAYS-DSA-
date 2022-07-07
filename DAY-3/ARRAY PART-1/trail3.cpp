2dead6084afeb85d4a9d1b94757978671e732cdc
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-07 23:21:01 +0530

    Create trail3.cpp

---
+++ DAY-3/ARRAY PART-1/trail3.cpp
@@ -0,0 +1,24 @@
+//sorted array naive approach 
+//use two for
+//time complexity is o(n^2)
+bool sort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++)
+          if(arr[i]>arr[j])
+           return false;
+
+    }
+    return true;
+}
+
+
+//efficient approach
+//time complexity is o(n)
+bool sort(int arr[],int n){
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1])
+          return false;
+    }
+    return true;
+}
+
