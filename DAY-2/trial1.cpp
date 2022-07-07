f5ba540f86360b42ea9a2793209563f1aeb53ff3
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-07 23:24:13 +0530

    Update trial1.cpp

--- DAY-2/trial1.cpp
+++ DAY-2/trial1.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #print the srings in lexicogrtaphic way
 
 #include<bits/stdc++.h>
@@ -16,4 +17,24 @@ int main()
     for(auto k:m){
         cout<<k.first<<" "<<k.second<<endl;
     }
+=======
+#print the srings in lexicogrtaphic way
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<string,int> m;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        m[s]=m[s]+1;
+
+    }
+    for(auto k:m){
+        cout<<k.first<<" "<<k.second<<endl;
+    }
+>>>>>>> 5c800002a20a931073c1389272d3833f0f1eac38
 }
