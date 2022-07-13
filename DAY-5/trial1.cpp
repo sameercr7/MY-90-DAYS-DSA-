9cf959a0381b8cfe8924ec23d5c3c813aec08dbe
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-13 13:20:54 +0530

    Update trial1.cpp

--- DAY-5/trial1.cpp
+++ DAY-5/trial1.cpp
@@ -1,2 +1,41 @@
-//sameer is a good boy
-//hat ja be bhaiii mere baat smjh
+// leader in an array
+//time complexity is o(n^2)
+//space complexity is o(1)
+// i/p ={7,20,4,10,6,5,2}
+// o/p 10 6 5 2
+//naive approach
+void leader(int arr[],int n){
+    for(int i=0;i<n;i++)
+    {
+        bool flag=false;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]<=arr[j]){
+                flag=true;
+                break;
+            }
+        }
+        if(flag==false)
+        print(arr[i]);
+    }
+}
+// efficient approach
+// in this approach printing is done from right to left as right most will always be a leader
+// time complexity is O(n)
+void leader(int arr[],int n){
+    // just copy the last elemnt in current leade and will be used in comparing
+
+     int current_leader=arr[n-1];
+     print(current_leader)
+
+    //  loop is very important do see it
+    for(int i=n-2;i>=0;i--)
+    {
+        if(current_leader<arr[i])
+        {
+            current_leader=arr[i]
+            print(current_leader);
+        }
+
+    }
+}
