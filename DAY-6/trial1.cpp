8d9e222bf20f76c75504eadbc9d8188ee2035a9a
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-14 23:53:21 +0530

    Update trial1.cpp

--- DAY-6/trial1.cpp
+++ DAY-6/trial1.cpp
@@ -1,6 +1,57 @@
 // TRAPPING RAINWATER PROBLEM
 //time complexity is o(n^2)
 //space complexity is o(1)
-// i/p ={7,20,4,10,6,5,2}
-// o/p 10 6 5 2
+// i/p ={1,2,3} or {3,2,1} as if it is in Inc/Dec then it will never hold any water so o/p = 0 
+// o/p      0          0
+//  i/p ={3,0,1,2,5}
+//  o/p =6
 //naive approach
+// for loop is starting with 1 and ending with n-1 bcoz we dont have ot hceck there as they are last block
+// we just take left max and right max for every i and take min of them an subtract it with block height
+#include<bits/stdc++.h>
+using namespace std;
+int getwater(int arr[],int n){
+    int res=0;
+    for(int i=1;i<n-1;i++){
+        int lmax=arr[i];
+        for(int j=0;j<i;j++)
+        {
+            lmax=max(lmax,arr[j]);
+        }
+        int rmax=arr[i];
+        for(int j=i+1;j<n;j++)
+        {
+            rmax=max(rmax,arr[j]);
+        }
+        res=res+(min(lmax,rmax)-arr[i]);
+    }
+    return res;
+}
+int main(){
+    int arr[]={3,0,1,2,5};
+    int c=getwater(arr,5);
+    cout<<c;
+    return 0;
+}
+// EFFIECIENT APPROACH
+//time complexity is o(n)
+#include<bits/stdc++.h>
+using namespace std;
+int getwater(int arr[],int n){
+    int res=0;
+    int lmax[n],rmax[n];
+    lmax[0]=arr[0];
+    for(int i=1;i<n;i++){
+        lmax=max(arr[i],arr[i-1]);
+    }
+    // its last element will be arrays last element
+
+    int rmax[n-1]=arr[n-1];
+    // here see the loop form where it is starting
+    for(int i=n-2;i>=0;i--){
+        rmax=max(arr[i],arr[i+1]);
+         }
+    for(int i=1;i<n-1;i++){
+        int res=res+(min(lmax[i],rmax[i])-arr[i]);
+    }
+}
