1bf0f599c04ddc97365f47d9d3f0146d7fe37caf
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-07-21 00:10:42 +0530

    Update trial2.cpp

--- DAY-6/trial2.cpp
+++ DAY-6/trial2.cpp
@@ -1,1 +1,18 @@
-// HI SAMEER YOU LOOK VERY GOOD
+// MAXIMUM SUBARRAY
+// //time complexity is o(n^2)
+//space complexity is o(1)
+// naive approach
+int maxSum(int arr[],int n)
+{
+    int res=arr[0];
+    for(int i=0;i<n;i++)
+    {
+        int current=0;
+        for(int j=i;j<n;j++)
+        {
+            current=current+arr[i];
+            res=max(res,current);
+        }
+    }
+    return res;
+}
