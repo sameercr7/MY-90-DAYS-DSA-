9a34cf88dc17e9293a381b3988aabafaf705fe3f
sameercr7 <64707041+sameercr7@users.noreply.github.com>
2022-08-09 21:58:48 +0530

    Update sameer.cpp

--- DAY-1/sameer.cpp
+++ DAY-1/sameer.cpp
@@ -8,14 +8,13 @@ int main(){
 
     cout<<"hello"<<" "<<a<<" "<<b;
 }
-#it only take only one value bcoz it is a character
-#456 7789
-o/p hello 456 7
-#return 0 sirf yhi btata h ki code ab complete hochuka h
-#in pass by reference if valu changes in function so it will also be changed in outside the fucntion 
-#in pass by value no changes in main function value
-#for more refecnce refer to screenshot
-=======
+//  it only take only one value bcoz it is a character
+// 456 7789
+// o/p hello 456 7
+// return 0 sirf yhi btata h ki code ab complete hochuka h
+//  pass by reference if value changes in function so it will also be changed in outside the fucntion 
+//  pass by value no changes in main function value
+// for more refecnce refer to screenshot
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
