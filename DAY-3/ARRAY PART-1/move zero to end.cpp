f8037f4ee6cd46851a58a99919e97d037b690791
sameer_cr7 <sameer05072001@gmail.com>
2022-09-11 11:27:17 +0530

    Update move zero to end.cpp

--- DAY-3/ARRAY PART-1/move zero to end.cpp
+++ DAY-3/ARRAY PART-1/move zero to end.cpp
@@ -31,6 +31,23 @@ for(int i=0;i<n;i++){
         count++;
     }
 }
+
+TWO pointer approach
+
+void moveZeroes(vector<int>& nums) {
+        int left=0,right=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[right]==0){
+                right++;
+            }
+            else{
+                swap(nums[right],nums[left]);
+            left++;
+            right++;
+            
+                
+        }
 =======
 //move zero to end
 //non negative  concept
