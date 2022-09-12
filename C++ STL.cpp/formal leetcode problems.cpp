4a98e4075ece0d256164be2ffc195ced98c6da9d
sameer_cr7 <sameer05072001@gmail.com>
2022-09-12 22:51:07 +0530

    Update formal leetcode problems.cpp

--- C++ STL.cpp/formal leetcode problems.cpp
+++ C++ STL.cpp/formal leetcode problems.cpp
@@ -1,2 +1,3 @@
 // 1-shuffle string
 // problem link -https://leetcode.com/problems/shuffle-string/
+// for reference -https://leetcode.com/problems/shuffle-string/discuss/773250/C%2B%2B-easy-O(N)-time-O(1)-space
