da28b683d46fd52c4525b0ab94483d493b4458c8
sameer_cr7 <sameer05072001@gmail.com>
2022-09-19 22:46:09 +0530

    Update formal leetcode problems.cpp

--- C++ STL.cpp/formal leetcode problems.cpp
+++ C++ STL.cpp/formal leetcode problems.cpp
@@ -1,3 +1,5 @@
 // 1-shuffle string
 // problem link -https://leetcode.com/problems/shuffle-string/
 // for reference -https://leetcode.com/problems/shuffle-string/discuss/773250/C%2B%2B-easy-O(N)-time-O(1)-space
+// first letter to appear twce
+// link-https://leetcode.com/problems/first-letter-to-appear-twice/
