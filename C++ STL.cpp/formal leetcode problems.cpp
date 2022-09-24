073795e424acac435248a6778e43edbfe42ef0bd
sameer_cr7 <sameer05072001@gmail.com>
2022-09-24 23:13:47 +0530

    Update formal leetcode problems.cpp

--- C++ STL.cpp/formal leetcode problems.cpp
+++ C++ STL.cpp/formal leetcode problems.cpp
@@ -1,5 +1,11 @@
-// 1-shuffle string
+1-
+// shuffle string
 // problem link -https://leetcode.com/problems/shuffle-string/
 // for reference -https://leetcode.com/problems/shuffle-string/discuss/773250/C%2B%2B-easy-O(N)-time-O(1)-space
+
+2
 // first letter to appear twce
 // link-https://leetcode.com/problems/first-letter-to-appear-twice/
+3-
+// find missing number
+// link-https://leetcode.com/problems/missing-number/
