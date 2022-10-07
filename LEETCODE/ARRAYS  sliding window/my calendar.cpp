573d08a4e7b235af681c263f81351c47302ef1d0
sameer_cr7 <sameer05072001@gmail.com>
2022-10-07 11:46:41 +0530

    Create my calendar.cpp

---
+++ LEETCODE/ARRAYS  sliding window/my calendar.cpp
@@ -0,0 +1,31 @@
+// link-https://leetcode.com/problems/my-calendar-iii/
+// youtube link-https://www.youtube.com/watch?v=8k2qVUv_a2M
+
+
+
+// approach
+// use map to solve this question
+// we have take mp[start]++ that means it will increment the key by 1 and mp[end]-- decrement the key
+// for example [10,20) 10->1,20->-1 so  finally  count becomes =0 and maxi is storing the maximum of the previous k events (i.e) maxi=1
+// rest you just dry run you will get my point more clearly
+
+
+class MyCalendarThree {
+public:
+    map<int,int> mp;
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int start, int end) {
+       mp[start]++;
+       mp[end]--; 
+       int count=0,maxi=0;
+       for(auto it:mp){
+        count+=it.second;
+        maxi=max(count,maxi);
+
+       }
+       return maxi;
+    }
+};
