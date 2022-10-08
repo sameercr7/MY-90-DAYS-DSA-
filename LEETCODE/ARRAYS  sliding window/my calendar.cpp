0df40f1409852ed08bf1dca26d3ce1f826a299f3
sameer_cr7 <sameer05072001@gmail.com>
2022-10-08 10:33:30 +0530

    Update my calendar.cpp

--- LEETCODE/ARRAYS  sliding window/my calendar.cpp
+++ LEETCODE/ARRAYS  sliding window/my calendar.cpp
@@ -4,10 +4,12 @@
 
 
 // approach
+// here we use map not unordered map because we want the events to be sorted order OR  we have to maintain the order of the events
 // use map to solve this question
 // we have take mp[start]++ that means it will increment the key by 1 and mp[end]-- decrement the key
 // for example [10,20) 10->1,20->-1 so  finally  count becomes =0 and maxi is storing the maximum of the previous k events (i.e) maxi=1
 // rest you just dry run you will get my point more clearly
+
 
 
 class MyCalendarThree {
