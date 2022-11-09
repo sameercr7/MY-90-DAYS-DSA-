5ba8967c84ada17f68a6037f323edb5fba2c0696
sameer_cr7 <sameer05072001@gmail.com>
2022-11-09 17:58:33 +0530

    Update make the string great.cpp

--- LEETCODE/STRINGS/make the string great.cpp
+++ LEETCODE/STRINGS/make the string great.cpp
@@ -1,6 +1,12 @@
 // link-https://leetcode.com/problems/make-the-string-great/
 // youtube link-https://www.youtube.com/watch?v=0Yfkz6ZBWE8
 
+
+// ans.back() ek peeche wale element ko check krta h
+// basic logic was if same character is in smaller Case  and also same char in uppercase then their Absolute difference should be 32
+// for e.g a=97 A=65 diffe=32
+
+// first by two loop
 class Solution {
 public:
     string makeGood(string s) {
@@ -16,6 +22,8 @@ public:
             
     }
 };
+
+// by the use of stack
 
 class Solution {
 public:
