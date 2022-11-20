15a5842e6513ee0a890b4371de408648af6da85a
sameer_cr7 <sameer05072001@gmail.com>
2022-11-20 21:32:03 +0530

    Update lowest common Ancestor for two given Nodes.cpp

--- LEETCODE/TREE/lowest common Ancestor for two given Nodes.cpp
+++ LEETCODE/TREE/lowest common Ancestor for two given Nodes.cpp
@@ -6,6 +6,8 @@
 // TC=o(N)
 // SC=O(N)
 
+
+
 class Solution {
 public:
     
@@ -25,5 +27,37 @@ public:
             return root;
             
         
+    }
+};
+
+
+// there is one approach also just calculaate the root to node path in vecotr array
+// and then just compare them 1,2,3,4,5 [1,2,6,7,8] jhna pe different aajaye uske ek phle wale ko return krdo
+
+
+
+1st Approach Using path arrays
+Time Complexity = O(N), Space Complexity = O(H) where H is the height of the Binary Tree
+In this solution we require three traversal of binary tree (0(N)+0(N)+0(N)) => 3(0(N)) => 0(N).
+
+class Solution {
+public:
+    bool findPath(TreeNode* root, vector<TreeNode*> &path, TreeNode* node){
+        if(root == NULL) return false;
+        path.push_back(root);
+        if(root == node) return true;
+        if(findPath(root->left, path, node) || findPath(root->right, path, node)) return true;
+        path.pop_back();
+        return false;
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> path1, path2;
+        if(findPath(root, path1, p) == false || findPath(root, path2, q) == false) return NULL;
+        int itr = 0;
+        for(itr; itr < min(path1.size(), path2.size()); itr++){
+            if(path1[itr] != path2[itr]) break;
+        }
+        return path1[itr-1];
     }
 };
